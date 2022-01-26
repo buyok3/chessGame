1ac625cfac5b9dd71c385b42f78c4afef24e1882
buyok3 <60290209+buyok3@users.noreply.github.com>
2022-01-26 10:43:33 +0200

    Added new piece(knight) and modified Board and BoardUtils

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,4 +1,7 @@
 package board;
 
 public class Board {
+    public Tile getTile(final int tileCoordinate) {
+        return null;
+    }
 }
--- src/board/BoardUtils.java
+++ src/board/BoardUtils.java
@@ -1,11 +1,27 @@
 package board;
 
 public class BoardUtils {
+    public static final boolean[] FIRST_COLUMN = initColumn(0);
+    public static final boolean[] SECOND_COLUMN = initColumn(1);
+    public static final boolean[] SEVENTH_COLUMN = initColumn(6);
+    public static final boolean[] EIGHTH_COLUMN = initColumn(7);
+
     public static final int TILES_NUMBER = 64;
     public static final int TILES_NUMBER_PER_ROW = 8;
 
     private BoardUtils(){
         throw new RuntimeException("You can't do that!");
+    }
+
+    private static boolean[] initColumn(int columnNumber){
+        final boolean[] column = new boolean[TILES_NUMBER];
+
+        do{
+            column[columnNumber] = true;
+            columnNumber += 8;
+        } while (columnNumber < TILES_NUMBER);
+
+        return column;
     }
 
     public static boolean isPossibleCoordinate(final int coordinate) {
